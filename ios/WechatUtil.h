f7efe5df5590bd1ad86ac8964851233c5b6939ba
执念 <gyb2020018@163.com>
2024-05-16 17:28:43 +0800

    :tada: 新增ios原生集成

--- ios/WechatUtil.h
+++ ios/WechatUtil.h
@@ -1,5 +1,33 @@
-#import <React/RCTBridgeModule.h>
+#import <Foundation/Foundation.h>
 
-@interface WechatUtil : NSObject <RCTBridgeModule>
+#import <React/RCTBridgeModule.h>
+#import <UIKit/UIKit.h>
+
+
+#import "WXApi.h"
+
+#define RCTWXShareTypeNews @"news"
+#define RCTWXShareTypeThumbImageUrl @"thumbImage"
+#define RCTWXShareTypeImageUrl @"imageUrl"
+#define RCTWXShareTypeImageFile @"imageFile"
+#define RCTWXShareTypeImageResource @"imageResource"
+#define RCTWXShareTypeText @"text"
+#define RCTWXShareTypeVideo @"video"
+#define RCTWXShareTypeAudio @"audio"
+#define RCTWXShareTypeFile @"file"
+
+#define RCTWXShareType @"type"
+#define RCTWXShareTitle @"title"
+#define RCTWXShareDescription @"description"
+#define RCTWXShareWebpageUrl @"webpageUrl"
+#define RCTWXShareImageUrl @"imageUrl"
+
+#define RCTWXEventName @"WeChat_Resp"
+#define RCTWXEventNameWeChatReq @"WeChat_Req"
+
+
+@interface WechatUtil : NSObject <RCTBridgeModule, WXApiDelegate>
+
+@property NSString* appId;
 
 @end
